7e92c4af4e12147fe504795fe65ad8496c8b3bc3
Unknown <Sebdami@hotmail.fr>
2018-04-18 12:30:48 +0200

    Create Explosion Class

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Explosion
+{
+    public float ExplosionRadius = 6.0f;
+    public float ExplosionForce = 10.0f;
+    public int Damage = 50;
+    public AnimationCurve DamageFalloff = AnimationCurve.Linear(0.0f, 1.0f, 1.0f, 0.0f);
+    public float UpLiftForce = 2.0f;
+}
--- Assets/Scripts/Projectile/ExplosiveProjectile.cs
+++ Assets/Scripts/Projectile/ExplosiveProjectile.cs
@@ -3,24 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplosiveProjectile : Projectile {
-    public float ExplosionRadius = 6.0f;
-    public float ExplosionForce = 10.0f;
-    public int Damage = 50;
-    public AnimationCurve DamageFalloff = AnimationCurve.Linear(0.0f, 1.0f, 1.0f, 0.0f);
-    public float UpLiftForce = 2.0f;
+    public Explosion explosion;
 
     public virtual void Explode()
     {
-        GameManager.instance.world.GetComponent<ModifyTerrain>().SphereAtPosition(transform.position, ExplosionRadius, 0);
-        Collider[] affectedColliders = Physics.OverlapSphere(transform.position, ExplosionRadius, LayerMask.GetMask("Worm"));
+        GameManager.instance.world.GetComponent<ModifyTerrain>().SphereAtPosition(transform.position, explosion.ExplosionRadius, 0);
+        Collider[] affectedColliders = Physics.OverlapSphere(transform.position, explosion.ExplosionRadius, LayerMask.GetMask("Worm"));
         foreach (Collider col in affectedColliders)
         {
             col.GetComponent<WormController>().CurrentState = WormState.Hit;
-            float damageFallOffMultiplier =DamageFalloff.Evaluate(Vector3.Distance(transform.position, col.transform.position) / ExplosionRadius);
+            float damageFallOffMultiplier = explosion.DamageFalloff.Evaluate(Vector3.Distance(transform.position, col.transform.position) / explosion.ExplosionRadius);
 
-            col.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce * damageFallOffMultiplier, transform.position, ExplosionRadius, UpLiftForce * damageFallOffMultiplier, ForceMode.Impulse);
+            col.GetComponent<Rigidbody>().AddExplosionForce(explosion.ExplosionForce * damageFallOffMultiplier, transform.position, explosion.ExplosionRadius, explosion.UpLiftForce * damageFallOffMultiplier, ForceMode.Impulse);
             // Apply damage with falloff
-            col.GetComponent<CharacterInstance>().CurrentHp -= (int)(Damage * damageFallOffMultiplier);
+            col.GetComponent<CharacterInstance>().CurrentHp -= (int)(explosion.Damage * damageFallOffMultiplier);
         }
 
         Destroy(gameObject);
