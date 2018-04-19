bceb3a72019529b41348d5316aa61d2920a29e76
Unknown <Sebdami@hotmail.fr>
2018-04-19 17:43:42 +0200

    Fix water rising right away

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -7,6 +7,11 @@ public class Water : MonoBehaviour {
     public float riseSpeed = 0.1f;
     bool isRising = false;
 
+    private void Start()
+    {
+        isRising = false;
+    }
+
     public void StartRising()
     {
         isRising = true;
@@ -14,6 +19,7 @@ public class Water : MonoBehaviour {
 
     private void Update()
     {
+        if(isRising)
         transform.position += Vector3.up * Time.deltaTime * riseSpeed;
     }
 }
