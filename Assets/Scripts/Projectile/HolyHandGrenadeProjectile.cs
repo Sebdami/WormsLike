bf0088184c80eb0d06b27f3016a572981ddc2c57
Unknown <Sebdami@hotmail.fr>
2018-04-20 15:12:15 +0200

    Add holy hand Grenade

---
+++ Assets/Scripts/Projectile/HolyHandGrenadeProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolyHandGrenadeProjectile : TimedExplosiveProjectile {
+
+    private new void Start()
+    {
+        base.Start();
+        Destroy(gameObject, 10.0f);
+    }
+
+    public override void Launch(Vector3 direction, float force)
+    {
+        base.Launch(direction, force);
+        rb.AddForce(Vector3.up * 2.0f);
+        GetComponent<Rigidbody>().AddTorque(Vector3.forward * force * 2.0f);
+    }
+
+    public override void Explode()
+    {
+        base.Explode();
+        isActive = false;
+        transform.GetChild(0).gameObject.SetActive(false);
+        Destroy(gameObject, 2.5f);
+        GetComponent<Collider>().enabled = false;
+        Destroy(GetComponent<Collider>());
+        Destroy(GetComponent<ConstantForce>());
+        Destroy(GetComponent<Rigidbody>());
+    }
+}
